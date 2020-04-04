87b2ae09c9db920ce25559ef9459239b05d3ddb1
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-04-04 17:00:44 -0400

    now it already compelted to ecmascript 8

--- src/es8/index.js
+++ src/es8/index.js
@@ -25,4 +25,30 @@ const string1 = 'hello';
 console.log(string1.padStart(7, 'hi'));
 console.log(string1.padEnd(12, '------'));
 
+console.log('food'.padEnd(12,'------'))
 
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(()=> resolve('hello world'), 3000)
+            : reject(new Error('Test error'))
+    })
+};
+
+const HelloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+HelloAsync();
+
+const anotherFunction = async () => {
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+anotherFunction();
