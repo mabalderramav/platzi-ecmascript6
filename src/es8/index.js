34a2ea1264e315694e3b6e8c5b9d683d8d600357
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-04-04 16:46:48 -0400

    completed ecmascript 8

---
+++ src/es8/index.js
@@ -0,0 +1,28 @@
+const data = {
+    fronted: 'aldo',
+    backend: 'oscar',
+    design: 'ana',
+}
+
+const entries = Object.entries(data);
+
+console.log(entries);
+
+console.log(entries.length);
+
+const data = {
+    fronted: 'aldo',
+    backend: 'oscar',
+    design: 'ana',
+}
+
+const values = Object.values(data);
+console.log(values);
+
+console.log(values.length);
+
+const string1 = 'hello';
+console.log(string1.padStart(7, 'hi'));
+console.log(string1.padEnd(12, '------'));
+
+
